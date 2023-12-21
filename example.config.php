85fa791bfca7c1bbdee1d9e9d4c7c8ef17fd3374
BrendonKoz <BrendonKoz@hotmail.com>
2023-12-21 15:38:31 -0500

    Update and rename config.php to example.config.php

--- example.config.php
+++ example.config.php
@@ -13,6 +13,9 @@ $timezone_str  = ''; // (https://www.php.net/manual/en/timezones.php)
 // Define constants
 const TOKEN_LIFETIME      = 60;           // minutes until invalid
 const TOKEN_FILE          = 'token';      // local file to store the API token
-const DATABASE_FILE       = 'db.sqlite3'; // local file to store the SQLite database
+const DATABASE_FILE       = __DIR__ . DIRECTORY_SEPARATOR . 'db.sqlite3'; // local file to store the SQLite database
 const DB_STRING_DELIMITER = '|';          // Seperates strings in combined TEXT fields
 const PDO_OPTIONS         = [PDO::ATTR_TIMEOUT => 0, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+// For demonstration of the examples:
+const CONTENT_URL         = 'https://example.com/libcal/';
