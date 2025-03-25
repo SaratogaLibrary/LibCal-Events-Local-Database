540773e5dde2cfaacb9c77eb59514d70209065f1
BrendonKoz <BrendonKoz@hotmail.com>
2025-03-25 15:29:31 -0400

    Update example.config.php
    Adding additional configuration options for the example projects.

--- example.config.php
+++ example.config.php
@@ -35,3 +35,8 @@ const GET_CHECKIN_STATUS         = true;
 const CONTENT_URL         = 'https://example.com/libcal/';
 const IMAGE_LOC           = CONTENT_URL . 'examples/library-svgrepo-com.svg';
 const BRAND_COLOR         = '#808080'; // must be defined in HTML HEX for the brochure example
+const PASSCODE            = '12345';
+
+// Time and Date Formats
+const SHORT_TIME_FORMAT   = 'g:ia';
+const LONG_DATE_FORMAT    = 'l, F j, Y';
